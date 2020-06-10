0f5f326b6856e9328160b1622c1929f040c3cb40
Ahmad A. Duhoki <ahmad.a.duhoki99@gmail.com>
2020-06-10 13:46:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const returnFirstTwoDrivers = function (arrOfDriver){
   let newArr = [arrOfDriver[0], arrOfDriver[1]];
   return newArr;
-}
+};
 
 const returnLastTwoDrivers = function (arrOfDriver) {
   let newArr = [arrOfDriver[arrOfDriver.length-2], arrOfDriver[arrOfDriver.length-1]];
   return newArr;
-}
+};
+
